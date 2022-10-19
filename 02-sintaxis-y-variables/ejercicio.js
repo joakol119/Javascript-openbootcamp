04768bf8f372b025faaf0725747ef08cf85aeb6e
joakol119 <96844494+joakol119@users.noreply.github.com>
2022-10-19 13:54:52 -0300

    Segunda tarea

---
+++ 02-sintaxis-y-variables/ejercicio.js
@@ -0,0 +1,25 @@
+let nombre = "Joaquin Poblete Esteves"
+let edad = "26"
+
+
+var eresDesarrolador = true;
+    if (eresDesarrolador){
+        console.log("Eres desarrollador");
+    }else{
+        console.log("No eres desarrollador");
+    }
+
+const fecha_birthday = new Date('1995-11-24');
+console.log(fecha_birthday);
+
+const libro={
+    titulo: 'La buena tierra',
+    autor: "Pearl S. Buck",
+    anyo: 1931,
+    genero: 'Novela',
+    paginas: 500,
+    editorial: 'Salamandra',
+    precio: 20,
+    url:'https://www.amazon.com/buena-tierra-Pearl-S-Buck/dp/8420677434',
+}
+console.log(libro.titulo)
