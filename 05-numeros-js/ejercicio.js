317ae2f4a6fd7c5e87f6ceb3f934e17b5032c50c
joakol119 <96844494+joakol119@users.noreply.github.com>
2022-10-26 13:34:36 -0300

    Ejercicio n°5

---
+++ 05-numeros-js/ejercicio.js
@@ -0,0 +1,10 @@
+let altura_cm = 175;
+let altura_metros = 1.75;
+let peso_kg = 68.2;
+
+console.log(altura_metros.toFixed(1));
+console.log(Math.floor(peso_kg));
+
+let max_js = Number.MAX_VALUE;
+
+console.log(max_js + 1);
